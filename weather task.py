a2a7f9a40b91c7259240e998c0bbf85d75fc3f6f
belalgamal553-spec <belalgamal553@gmail.com>
2025-09-02 16:32:36 +0300

    Add files via upload

---
+++ weather task.py
@@ -0,0 +1,10 @@
+print("Hello, ")
+int_celsius = int(input("Enter temperature in Celsius: "))
+if int_celsius >= 30 :
+    print("it's a hot day . atsay hydrated")
+elif int_celsius >= 20 :
+    print("it's a warm day . enjoy your weather")
+elif int_celsius >= 10 :
+    print("it's a cold day . wear a jacket")
+else :
+    print("it's a cold day . stay warm")
